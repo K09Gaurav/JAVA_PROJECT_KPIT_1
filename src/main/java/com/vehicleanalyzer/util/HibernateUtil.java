46408dc936dcf1a63a5a4b845792a800a05a707e
Kumar Gaurav <k09gaurav@gmail.com>
2025-04-11 17:08:10 +0530

    Added hibernate.cfg.xml and HibernateUtil.java

--- src/main/java/com/vehicleanalyzer/util/HibernateUtil.java
+++ src/main/java/com/vehicleanalyzer/util/HibernateUtil.java
@@ -1,5 +1,23 @@
 package com.vehicleanalyzer.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
-    
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory(){
+
+        if (sessionFactory == null){
+            try {
+                Configuration configuration = new Configuration();
+                configuration.configure("hibernate.cfg.xml");
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+
+
+        return sessionFactory;
+    }
 }
